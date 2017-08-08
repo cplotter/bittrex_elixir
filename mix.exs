f5f920f8a340309a1584fd1629c4d0603b45e531
Casper Lotter <casper@platform45.com>
2017-08-09 01:29:56 +0200

    Set versions for for poison and httppoison

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule BittrexElixir.Mixfile do
 
   def project do
     [app: :bittrex_elixir,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -19,8 +19,8 @@ defmodule BittrexElixir.Mixfile do
 
   defp deps do
     [
-      {:httpoison, github: "edgurgel/httpoison", tag: :master},
-      {:poison, github: "devinus/poison", tag: :master}
+      {:httpoison, "~> 0.13.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
